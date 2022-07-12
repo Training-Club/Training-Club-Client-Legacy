43d082451d3e7d04a59f454e50eedc039d1fbbf5
John Rockwell <john@llewkcor.com>
2022-07-12 13:44:45 -0700

    main: implemented renameable header in the Current Session Screen

--- src/components/screens/training/CurrentSessionScreen.tsx
+++ src/components/screens/training/CurrentSessionScreen.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
-import {Heading, View} from 'native-base';
+import React, {useCallback} from 'react';
+import {Box, View} from 'native-base';
 import {useSessionContext} from '../../../context/session/SessionContext';
+import RenameableHeader from '../../molecules/training/RenameableHeader';
 
 const CurrentSessionScreen = (): JSX.Element => {
   const {draft, setDraft} = useSessionContext();
 
+  const sessionName = draft?.sessionName ?? 'My Workout';
+  const spacing = 4;
+
   React.useEffect(() => {
+    // Initializes a new training session if no existing draft is running
     if (!draft) {
       setDraft({sessionName: 'My Workout', timestamp: new Date()});
     }
   }, [draft, setDraft]);
 
+  const handleSessionNameChange = useCallback(
+    (name: string) => {
+      setDraft({...draft, sessionName: name});
+    },
+    [draft, setDraft],
+  );
+
   return (
-    <View>
-      {draft && draft.sessionName && <Heading>{draft.sessionName}</Heading>}
+    <View px={spacing}>
+      <Box w={'100%'} mt={spacing}>
+        <RenameableHeader
+          text={sessionName}
+          setText={handleSessionNameChange}
+          options={{maxLength: 24}}
+          closeButton={{
+            stackName: 'Main',
+            screenName: 'Home',
+          }}
+        />
+      </Box>
     </View>
   );
 };

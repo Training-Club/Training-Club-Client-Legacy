a4ff6c448ecd9900f08ab5945c56f639ea8902d0
John Rockwell <john@llewkcor.com>
2022-07-12 13:42:46 -0700

    main: implemented RenameableHeader component

---
+++ src/components/molecules/training/RenameableHeader.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import {HStack, IconButton, Input, useColorModeValue} from 'native-base';
+import {default as MaterialCommunityIcons} from 'react-native-vector-icons/MaterialCommunityIcons';
+import {ColorType} from 'native-base/lib/typescript/components/types';
+import {useNavigation} from '@react-navigation/core';
+
+interface IRenameableHeaderProps {
+  text: string;
+  setText: (s: string) => void;
+
+  options?: {
+    maxLength?: number;
+  };
+
+  closeButton?: {
+    backgroundColor?: ColorType;
+    pressedBackgroundColor?: ColorType;
+    iconColor?: ColorType;
+    stackName: string;
+    screenName: string;
+  };
+}
+
+const RenameableHeader = ({
+  text,
+  setText,
+  options,
+  closeButton,
+}: IRenameableHeaderProps): JSX.Element => {
+  const navigation = useNavigation();
+  const closeButtonStackName = closeButton?.stackName ?? 'Main';
+  const closeButtonScreenName = closeButton?.screenName ?? 'Home';
+
+  const defaultCloseButtonBackgroundColor = useColorModeValue(
+    'apple.gray.100',
+    'apple.gray.800',
+  );
+
+  const defaultCloseButtonPressedBackgroundColor = useColorModeValue(
+    'apple.gray.300',
+    'apple.gray.600',
+  );
+
+  const defaultCloseButtonIconColor = useColorModeValue(
+    'apple.gray.800',
+    'apple.gray.100',
+  );
+
+  return (
+    <HStack
+      testID={'renameable-header'}
+      w={'100%'}
+      justifyContent={'space-between'}>
+      <Input
+        variant={'unstyled'}
+        size={'2xl'}
+        fontWeight={'bold'}
+        w={'100%'}
+        pl={0}
+        maxLength={options?.maxLength}
+        defaultValue={text}
+        onBlur={e => setText(e.nativeEvent.text)}
+      />
+
+      {closeButton && (
+        <IconButton
+          testID={'renameable-header-close-btn'}
+          position={'absolute'}
+          right={0}
+          size={'sm'}
+          rounded={'full'}
+          variant={'solid'}
+          bgColor={
+            closeButton.backgroundColor ?? defaultCloseButtonBackgroundColor
+          }
+          _pressed={{
+            bgColor:
+              closeButton.pressedBackgroundColor ??
+              defaultCloseButtonPressedBackgroundColor,
+          }}
+          _icon={{
+            as: MaterialCommunityIcons,
+            name: 'window-close',
+            color: closeButton.iconColor ?? defaultCloseButtonIconColor,
+          }}
+          onPress={() => {
+            navigation.navigate(
+              closeButtonStackName as never,
+              {screen: closeButtonScreenName} as never,
+            );
+          }}
+        />
+      )}
+    </HStack>
+  );
+};
+
+export default React.memo(RenameableHeader);

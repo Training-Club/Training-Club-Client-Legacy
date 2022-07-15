28d443a8482acbfb497826c63970c028dff6bba2
John Rockwell <john@llewkcor.com>
2022-07-14 22:20:00 -0700

    main: implemented exercise search list

--- src/components/molecules/training/ExerciseSearchResult.tsx
+++ src/components/molecules/training/ExerciseSearchResult.tsx
@@ -1,19 +1,46 @@
 import React from 'react';
-import {Box, Heading, HStack} from 'native-base';
-import {ExerciseInfo} from '../../../models/Training';
+import {
+  Box,
+  Heading,
+  HStack,
+  Pressable,
+  Text,
+  useColorModeValue,
+  VStack,
+} from 'native-base';
 
 interface IExerciseSearchResultProps {
-  data: ExerciseInfo;
+  title: string;
+  subtitle?: string;
+  leftContent: JSX.Element;
+  onPress?: () => void;
 }
 
 const ExerciseSearchResult = ({
-  data,
+  title,
+  subtitle,
+  leftContent,
+  onPress,
 }: IExerciseSearchResultProps): JSX.Element => {
+  const textColor = useColorModeValue('black', 'white');
+  const textColorMuted = useColorModeValue('apple.gray.600', 'apple.gray.400');
+
   return (
-    <HStack w={'100%'}>
-      <Box w={'50px'} h={'50px'} bgColor={'blue.500'} />
-      <Heading>{data.name}</Heading>
-    </HStack>
+    <Pressable onPressIn={onPress}>
+      <HStack w={'100%'} alignItems={'center'} space={4} py={1}>
+        <Box w={'64px'} h={'64px'}>
+          {leftContent}
+        </Box>
+
+        <VStack>
+          <Heading size={'md'} color={textColor}>
+            {title}
+          </Heading>
+
+          {subtitle && <Text color={textColorMuted}>{subtitle}</Text>}
+        </VStack>
+      </HStack>
+    </Pressable>
   );
 };
 
--- src/components/organisms/training/ExerciseSearchResultList.tsx
+++ src/components/organisms/training/ExerciseSearchResultList.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import {ExerciseInfo} from '../../../models/Training';
 import ExerciseSearchResult from '../../molecules/training/ExerciseSearchResult';
-import {Box, ScrollView} from 'native-base';
+import {default as MaterialIcons} from 'react-native-vector-icons/MaterialIcons';
+import {useNavigation} from '@react-navigation/core';
+import {Capitalize} from '../../../utils/StringUtil';
+import {
+  Avatar,
+  Box,
+  Flex,
+  Icon,
+  ScrollView,
+  useColorModeValue,
+} from 'native-base';
 
 interface IExerciseSearchResultListProps {
   data: ExerciseInfo[];
@@ -10,11 +20,62 @@ interface IExerciseSearchResultListProps {
 const ExerciseSearchResultList = ({
   data,
 }: IExerciseSearchResultListProps): JSX.Element => {
+  const navigation = useNavigation();
+  const avatarColor = useColorModeValue('apple.blue.light', 'apple.red.dark');
+  const textColor = useColorModeValue('black', 'white');
+
+  const LeftContent = (exercise: ExerciseInfo): JSX.Element => {
+    return (
+      <Avatar size={'lg'} bgColor={avatarColor}>
+        {exercise.name.charAt(0).toUpperCase()}
+      </Avatar>
+    );
+  };
+
+  const CreateNewContent = (): JSX.Element => {
+    return (
+      <Flex
+        justifyContent={'center'}
+        alignItems={'center'}
+        w={'100%'}
+        h={'100%'}
+        bgColor={avatarColor}
+        rounded={'full'}>
+        <Icon as={MaterialIcons} name={'add'} size={8} color={textColor} />
+      </Flex>
+    );
+  };
+
+  const handleCreateNewContent = React.useCallback(() => {
+    navigation.navigate(
+      'Training' as never,
+      {screen: 'CreateExercise'} as never,
+    );
+  }, [navigation]);
+
   return (
-    <Box w={'100%'} bgColor={'red.500'}>
+    <Box w={'100%'} py={2}>
       <ScrollView w={'100%'}>
+        <ExerciseSearchResult
+          title={'Create New Exercise'}
+          onPress={() => handleCreateNewContent()}
+          leftContent={CreateNewContent()}
+        />
+
         {data.map((exercise, i) => {
-          return <ExerciseSearchResult key={`esr-${i}`} data={exercise} />;
+          return (
+            <ExerciseSearchResult
+              key={`esr-${i}`}
+              title={exercise.name}
+              subtitle={
+                exercise.exerciseEquipment
+                  ? Capitalize(exercise.exerciseEquipment)
+                  : undefined
+              }
+              onPress={() => console.log(exercise.name)}
+              leftContent={LeftContent(exercise)}
+            />
+          );
         })}
       </ScrollView>
     </Box>

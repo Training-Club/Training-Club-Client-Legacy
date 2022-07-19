4bb741afe80d3cc63e4f7f3b43d9920ceb5b45d4
John Rockwell <john@llewkcor.com>
2022-07-18 21:47:57 -0700

    main: updated exercise context provider to have state altering functions

--- src/context/exercise/ExerciseContext.Provider.tsx
+++ src/context/exercise/ExerciseContext.Provider.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {IExercise} from '../../models/Training';
+import {GroupedExercise, IExercise} from '../../models/Training';
 import {ExerciseContext} from './ExerciseContext';
+import {nanoid} from 'nanoid/non-secure';
 
 interface IExerciseContextProviderProps {
   children: any;
@@ -11,11 +12,97 @@ export function ExerciseContextProvider({
 }: IExerciseContextProviderProps) {
   const [exercises, setExercises] = React.useState<IExercise[]>([]);
 
+  /**
+   * Duplicates the provided exercise and adds it to state
+   * @param {IExercise} exercise Exercise to duplicate
+   */
+  const duplicateSet = React.useCallback(
+    (exercise: IExercise) => {
+      const copiedExercises: IExercise[] = [...exercises];
+      const copy: IExercise = {
+        ...exercise,
+        id: nanoid(5),
+        addedAt: new Date(),
+        performed: false,
+      };
+
+      copiedExercises.push(copy);
+
+      requestAnimationFrame(() => {
+        setExercises(copiedExercises);
+      });
+    },
+    [exercises],
+  );
+
+  /**
+   * Removes the provided exercise set from state
+   * @param {IExercise} exercise Exercise to remove
+   */
+  const removeSet = React.useCallback(
+    (exercise: IExercise) => {
+      requestAnimationFrame(() => {
+        setExercises(exercises.filter(e => e.id !== exercise.id));
+      });
+    },
+    [exercises],
+  );
+
+  /**
+   * Toggles the provided exercise's completion state and sets it in state
+   * @param exercise
+   */
+  const toggleComplete = React.useCallback(
+    (exercise: IExercise) => {
+      const copy = exercises.map(e =>
+        exercise.id !== e.id ? e : {...e, performed: !e.performed},
+      );
+
+      requestAnimationFrame(() => {
+        setExercises(copy);
+      });
+    },
+    [exercises],
+  );
+
+  /**
+   * Adds the provided exercise to state
+   * @param {IExercise} exercise Exercise to add
+   */
+  const addExercise = React.useCallback(
+    (exercise: IExercise) => {
+      requestAnimationFrame(() => {
+        setExercises([...exercises, exercise]);
+      });
+    },
+    [exercises],
+  );
+
+  /**
+   * Removes the provided grouped exercise (and all exercises under it) from the state
+   * @param {GroupedExercise} groupedExercise Exercise (by name) to remove
+   */
+  const removeExercise = React.useCallback(
+    (groupedExercise: GroupedExercise) => {
+      requestAnimationFrame(() => {
+        setExercises([
+          ...exercises.filter(e => e.exerciseName !== groupedExercise.name),
+        ]);
+      });
+    },
+    [exercises],
+  );
+
   return (
     <ExerciseContext.Provider
       value={{
         exercises,
         setExercises: (e: IExercise[]) => setExercises(e),
+        duplicateSet: (e: IExercise) => duplicateSet(e),
+        removeSet: (e: IExercise) => removeSet(e),
+        toggleComplete: (e: IExercise) => toggleComplete(e),
+        addExercise: (e: IExercise) => addExercise(e),
+        removeExercise: (e: GroupedExercise) => removeExercise(e),
       }}>
       {children}
     </ExerciseContext.Provider>
--- src/context/exercise/ExerciseContext.ts
+++ src/context/exercise/ExerciseContext.ts
@@ -1,14 +1,24 @@
 import React from 'react';
-import {IExercise} from '../../models/Training';
+import {GroupedExercise, IExercise} from '../../models/Training';
 
 export interface IExerciseContext {
   exercises: IExercise[];
   setExercises: (exercises: IExercise[]) => void;
+  duplicateSet: (exercise: IExercise) => void;
+  removeSet: (exercise: IExercise) => void;
+  toggleComplete: (exercise: IExercise) => void;
+  addExercise: (exercise: IExercise) => void;
+  removeExercise: (groupedExercise: GroupedExercise) => void;
 }
 
 export const ExerciseContext = React.createContext<IExerciseContext>({
   exercises: [],
   setExercises: () => {},
+  duplicateSet: () => {},
+  removeSet: () => {},
+  toggleComplete: () => {},
+  addExercise: () => {},
+  removeExercise: () => {},
 });
 
 export const useExerciseContext = () => React.useContext(ExerciseContext);

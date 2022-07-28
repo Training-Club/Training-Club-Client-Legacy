fb49df421f614bdbd90283c1bf7f6697036d88a4
John Rockwell <john@llewkcor.com>
2022-07-27 19:05:00 -0700

    main: implemented test cases for exercise additional search screen

---
+++ __tests__/ExerciseAdditionalSearchScreen.test.tsx
@@ -0,0 +1,63 @@
+import theme from '../src/Theme';
+import {NavigationContainer} from '@react-navigation/native';
+import {NativeBaseProvider, View} from 'native-base';
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import AdditionalExerciseSelection from '../src/components/molecules/training/AdditionalExerciseSelection';
+import {
+  ExerciseEquipment,
+  ExerciseInfo,
+  ExerciseType,
+  MuscleGroup,
+} from '../src/models/Training';
+
+describe('Exercise Additional Search Screen ->', () => {
+  const inset = {
+    frame: {x: 0, y: 0, width: 0, height: 0},
+    insets: {top: 0, left: 0, right: 0, bottom: 0},
+  };
+
+  it('should render selection content', () => {
+    const exerciseData: ExerciseInfo[] = [
+      {
+        id: '0',
+        name: 'Benchpress',
+        type: ExerciseType.WEIGHTED_REPS,
+        verified: true,
+        muscleGroups: [MuscleGroup.CHEST, MuscleGroup.TRICEPS],
+        exerciseEquipment: ExerciseEquipment.BARBELL,
+      },
+
+      {
+        id: '1',
+        name: 'Back Squat',
+        type: ExerciseType.WEIGHTED_REPS,
+        verified: true,
+        muscleGroups: [MuscleGroup.QUADS, MuscleGroup.HAMSTRING],
+        exerciseEquipment: ExerciseEquipment.BARBELL,
+      },
+    ];
+
+    const {getByText} = render(
+      <NativeBaseProvider initialWindowMetrics={inset} theme={theme()}>
+        <NavigationContainer>
+          <View w={'100%'}>
+            <AdditionalExerciseSelection
+              data={exerciseData}
+              onToggle={jest.fn}
+              onComplete={jest.fn}
+            />
+          </View>
+        </NavigationContainer>
+      </NativeBaseProvider>,
+    );
+
+    const benchPill = getByText('Benchpress');
+    const squatPill = getByText('Back Squat');
+    const addBtnPill = getByText('Add');
+
+    expect(benchPill).toBeTruthy();
+    expect(squatPill).toBeTruthy();
+    expect(addBtnPill).toBeTruthy();
+  });
+});

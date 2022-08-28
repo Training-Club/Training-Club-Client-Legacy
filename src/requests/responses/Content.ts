45aa4ad2404f3af21259a034d4c2c7f00e984a51
John Rockwell <john@llewkcor.com>
2022-08-27 19:56:13 -0700

    main: added skeleton for content response types

---
+++ src/requests/responses/Content.ts
@@ -0,0 +1,31 @@
+import {IPost} from '../../models/Content';
+
+export type GetPostsByQueryResponse = {
+  result: IPost[];
+};
+
+export type GetCommentsByPostIdResponse = {};
+
+export type GetCommentCountResponse = {};
+
+export type GetLikeCountResponse = {};
+
+export type GetLikeListResponse = {};
+
+export type CreatePostResponse = {
+  message: string;
+};
+
+export type CreateCommentResponse = {};
+
+export type AddLikeResponse = {};
+
+export type RemoveLikeResponse = {};
+
+export type UpdatePostResponse = {};
+
+export type UpdateCommentResponse = {};
+
+export type DeletePostResponse = {};
+
+export type DeleteCommentResponse = {};

6104ed2aa5fd34f35254250ec9a164fb6b55b6b4
j-rockwell <john@llewkcor.com>
2022-11-04 11:35:14 -0700

    bug/34: fixed bottom margin and implemented exempt small devices using device info

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -1,10 +1,7 @@
 import {extendTheme} from 'native-base';
-import {Dimensions} from 'react-native';
+import {isSmallScreen} from './utils/DeviceUtil';
 
 export default function () {
-  const {width} = Dimensions.get('screen');
-  const smallScreen = width <= 375;
-
   return extendTheme({
     config: {
       useSystemColorMode: true,
@@ -71,7 +68,7 @@ export default function () {
                 : 'core.background.light',
             width: '100%',
             height: '100%',
-            pt: smallScreen ? '16px' : '32px',
+            pt: isSmallScreen() ? '16px' : '32px',
           };
         },
       },

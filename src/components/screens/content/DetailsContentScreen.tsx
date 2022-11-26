ac854d5015885bc056bf169b3d9e668bc3d84213
j-rockwell <john@llewkcor.com>
2022-11-25 21:55:31 -0800

    feature/discovery: added privacy level and implemented reuseable chip component

--- src/components/screens/content/DetailsContentScreen.tsx
+++ src/components/screens/content/DetailsContentScreen.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import {FormatTrainingSessionQuery} from '../../../utils/StringUtil';
 import {getTrainingSessions} from '../../../requests/Training';
 import {usePushdownContext} from '../../../context/pushdown/PushdownContext';
 import {useContentDraftContext} from '../../../context/content/ContentDraftContext';
 import {createPostWithFiles} from '../../../requests/Content';
 import {useNavigation} from '@react-navigation/native';
+import {Chip} from '../../atoms/design/Chip';
+import SegmentedControl from '@react-native-segmented-control/segmented-control';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {PrivacyLevel} from '../../../models/Privacy';
+import {ILocation, LocationType} from '../../../models/Location';
 import CloseableHeader from '../../molecules/design/CloseableHeader';
 import useAccountStore from '../../../store/AccountStore';
-import {default as MaterialIcons} from 'react-native-vector-icons/MaterialIcons';
 
-import Animated, {
-  SequencedTransition,
-  FadeOut,
-  SlideInRight,
-} from 'react-native-reanimated';
-
-import {ILocation, LocationType} from '../../../models/Location';
+import {
+  Capitalize,
+  FormatTrainingSessionQuery,
+} from '../../../utils/StringUtil';
 
 import {
   ITrainingSession,
@@ -31,12 +29,9 @@ import {
   ScrollView,
   Select,
   TextArea,
-  Text,
   View,
-  Square,
-  Pressable,
   HStack,
-  Icon,
+  Center,
   useColorModeValue,
   Input,
 } from 'native-base';
@@ -50,6 +45,7 @@ const DetailsContentScreen = (): JSX.Element => {
   const navigation = useNavigation<NativeStackNavigationProp<any>>();
   const [caption, setCaption] = React.useState('');
   const [tags, setTags] = React.useState<string[]>([]);
+  const [privacyLevel, setPrivacyLevel] = React.useState(PrivacyLevel.PUBLIC);
 
   const [trainingSessions, setTrainingSessions] = React.useState<
     ITrainingSession[]
@@ -74,13 +70,19 @@ const DetailsContentScreen = (): JSX.Element => {
   ]);
 
   const textColor = useColorModeValue('core.text.light', 'core.text.dark');
+  const inputBgColor = useColorModeValue('apple.gray.50', 'apple.gray.900');
+  const segmentedTextColor = useColorModeValue('black', 'white');
+  const segmentedSelectedTextColor = useColorModeValue('black', 'black');
+
+  const segmentedBgColor = useColorModeValue(
+    'core.backgroundAccent.light',
+    'core.backgroundAccent.dark',
+  );
 
   const mutedTextColor = useColorModeValue(
     'core.mutedText.light',
     'core.mutedText.dark',
   );
-
-  const inputBgColor = useColorModeValue('apple.gray.50', 'apple.gray.900');
 
   /**
    * Handles adding single or multiple tags to the current tags
@@ -107,6 +109,45 @@ const DetailsContentScreen = (): JSX.Element => {
   const onTagRemove = React.useCallback((tag: string) => {
     setTags(prevState => prevState.filter(t => t !== tag));
   }, []);
+
+  /**
+   * Returns an array of the privacy levels in user-readable format
+   */
+  const getPrivacyLevels = React.useCallback(() => {
+    let values: string[] = [];
+
+    Object.keys(PrivacyLevel).forEach(pl => {
+      values.push(Capitalize(pl.replace('_', ' ')));
+    });
+
+    return values;
+  }, []);
+
+  /**
+   * Callback function that updates state when the privacy
+   * level is updated.
+   */
+  const handlePrivacyLevelChange = React.useCallback(
+    (value: string) => {
+      if (value === 'Public' && privacyLevel !== PrivacyLevel.PUBLIC) {
+        setPrivacyLevel(PrivacyLevel.PUBLIC);
+        return;
+      }
+
+      if (
+        value === 'Followers Only' &&
+        privacyLevel !== PrivacyLevel.FOLLOWERS_ONLY
+      ) {
+        setPrivacyLevel(PrivacyLevel.FOLLOWERS_ONLY);
+        return;
+      }
+
+      if (value === 'Private' && privacyLevel !== PrivacyLevel.PRIVATE) {
+        setPrivacyLevel(PrivacyLevel.PRIVATE);
+      }
+    },
+    [privacyLevel],
+  );
 
   /**
    * Handles submitting the POST request to the server to create the post
@@ -201,12 +242,45 @@ const DetailsContentScreen = (): JSX.Element => {
             />
           </FormControl>
 
-          <FormControl mt={8}>
+          <FormControl w={'100%'} mt={8}>
+            <FormControl.Label>Post Privacy</FormControl.Label>
+
+            <SegmentedControl
+              values={getPrivacyLevels()}
+              selectedIndex={0}
+              onValueChange={value => handlePrivacyLevelChange(value)}
+              backgroundColor={segmentedBgColor}
+              fontStyle={{color: segmentedTextColor}}
+              activeFontStyle={{color: segmentedSelectedTextColor}}
+            />
+
+            <Center>
+              {privacyLevel === PrivacyLevel.PUBLIC && (
+                <FormControl.HelperText>
+                  Your post will be visible to any member of Training Club.
+                </FormControl.HelperText>
+              )}
+
+              {privacyLevel === PrivacyLevel.FOLLOWERS_ONLY && (
+                <FormControl.HelperText>
+                  Your post will only be visible to those who follow you.
+                </FormControl.HelperText>
+              )}
+
+              {privacyLevel === PrivacyLevel.PRIVATE && (
+                <FormControl.HelperText>
+                  Your post will only be visble to you.
+                </FormControl.HelperText>
+              )}
+            </Center>
+          </FormControl>
+
+          <FormControl mt={4}>
             <FormControl.Label color={mutedTextColor}>
               Attach a Training Session
             </FormControl.Label>
 
-            <Select>
+            <Select bgColor={inputBgColor} borderRadius={12}>
               {trainingSessions &&
                 trainingSessions.map(session => (
                   <Select.Item
@@ -223,7 +297,7 @@ const DetailsContentScreen = (): JSX.Element => {
               Location
             </FormControl.Label>
 
-            <Select>
+            <Select bgColor={inputBgColor} borderRadius={12}>
               {locations &&
                 locations.map(location => (
                   <Select.Item
@@ -238,40 +312,16 @@ const DetailsContentScreen = (): JSX.Element => {
           <FormControl mt={4}>
             <FormControl.Label color={mutedTextColor}>Tags</FormControl.Label>
 
-            <Input onSubmitEditing={e => onTagAdd(e.nativeEvent.text)} />
+            <Input
+              bgColor={inputBgColor}
+              borderRadius={12}
+              onSubmitEditing={e => onTagAdd(e.nativeEvent.text)}
+            />
 
             {tags && (
               <HStack space={2} mt={2} flexWrap={'wrap'}>
                 {tags.map(tag => (
-                  <Animated.View
-                    entering={SlideInRight}
-                    layout={SequencedTransition.delay(300)}
-                    exiting={FadeOut.duration(250)}>
-                    <Pressable onPress={() => onTagRemove(tag)}>
-                      <Square
-                        bgColor={'black'}
-                        mt={2}
-                        p={1}
-                        minW={16}
-                        borderRadius={'full'}>
-                        <HStack
-                          space={1}
-                          justifyContent={'center'}
-                          alignItems={'center'}>
-                          <Text fontSize={'xs'} color={'white'}>
-                            {tag}
-                          </Text>
-
-                          <Icon
-                            as={MaterialIcons}
-                            name={'close'}
-                            size={4}
-                            color={'white'}
-                          />
-                        </HStack>
-                      </Square>
-                    </Pressable>
-                  </Animated.View>
+                  <Chip text={tag} onPress={() => onTagRemove(tag)} />
                 ))}
               </HStack>
             )}
@@ -279,12 +329,14 @@ const DetailsContentScreen = (): JSX.Element => {
         </ScrollView>
       </Box>
 
-      <Box w={'100%'} position={'absolute'} bottom={0} left={0} mb={8} px={4}>
+      <Box position={'absolute'} left={0} bottom={8} px={4} w={'100%'}>
         <Button
+          onPress={() => onSubmit()}
           variant={'info'}
-          _text={{color: 'white'}}
-          onPress={() => onSubmit()}>
-          Create
+          size={'lg'}
+          w={'100%'}
+          _text={{color: 'white'}}>
+          Submit
         </Button>
       </Box>
     </View>
